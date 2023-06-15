d94b0913e0abd42b0e4f08bf9ded8a32afcdd124
İbrahim Cem Keleş <118098275+ibrahimcemk@users.noreply.github.com>
2023-06-15 21:00:42 +0300

    Add files via upload

---
+++ arduino-analog-okuma-ve-seri-haberlesme.ino
@@ -0,0 +1,20 @@
+#define potpin A0
+
+int deger=0;
+
+void setup() {
+ 
+ Serial.begin(9600);
+ Serial.println("Pot Değer Okuma");
+
+}
+
+void loop() {
+ 
+ deger = analogRead(potpin);
+  
+ float gerilim = (5.00/1024.00)*deger;
+ Serial.println(gerilim);
+ Serial.println(deger);
+ delay(260);
+}
